66c361170dcf7a654663d4e6822218ae51e914df
Hossein Amirabdollahi <hosseinamirabdollahi8@gmail.com>
2024-11-20 19:36:02 +0330

    add Passenger model

--- airline/urls.py
+++ airline/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    # path("flights", include("flights/urls   ")),
+    path("flights/", include("flights.urls")),
 ]
